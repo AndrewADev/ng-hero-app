c881e26cfdade736ba11b5c5cd795c0073ffccda
Andrew A <AndrewADev@gmail.com>
2018-01-15 19:30:53 -0500

    Fix HeroDetail tests

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeroDetailComponent } from './hero-detail.component';
@@ -8,6 +9,7 @@ describe('HeroDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [FormsModule],
       declarations: [ HeroDetailComponent ]
     })
     .compileComponents();
