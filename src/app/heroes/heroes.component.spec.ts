5d411aba37d000cf51672c4764ac29c1b3f9ccb2
Andrew A <AndrewADev@gmail.com>
2018-06-24 14:25:15 -0400

    Fix HeroComponent tests

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,41 +1,51 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Component, Input } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
+import { RouterLinkDirectiveStub } from '../../testing/router-link-directive-stub';
+
+import { Hero } from '../hero';
 import { HeroesComponent } from './heroes.component';
 import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 import { HeroService } from '../hero.service';
 
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
-  let heroService: HeroService;
   let heroServiceStub: {};
+  const emptyHeroes: Hero[] = [];
+  // TODO: better typing?
+  let linkDes: any;
+  let routerLinks: any[] = [];
 
   let fixture: ComponentFixture<HeroesComponent>;
 
   beforeEach((async() => {
     heroServiceStub = {
-      getHeroes(): void {}
+      getHeroes(): Observable<Hero[]> { return of(emptyHeroes as Hero[]); }
     };
 
     TestBed.configureTestingModule({
       declarations: [
         HeroesComponent,
         MockHeroDetailComponent,
+        RouterLinkDirectiveStub,
        ],
        providers: [
          {provide: HeroService, useValue: heroServiceStub}
        ]
     })
     .compileComponents();
-
-    heroService = TestBed.get(HeroService);
-
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HeroesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    linkDes = fixture.debugElement.queryAll(By.directive(RouterLinkDirectiveStub));
+
+    routerLinks = linkDes.map(de => de.injector.get(RouterLinkDirectiveStub));
   });
 
   it('should create', () => {
