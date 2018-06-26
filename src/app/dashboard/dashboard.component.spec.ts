ae7082ba01a7808508fbddfb8a98afbe35f248c4
Andrew A <AndrewADev@gmail.com>
2018-06-25 20:39:36 -0400

    Fix dashboard component tests
    Fix tests for dashboard component. Add a stub class HeroServiceStub to simplify testing (keep creating same stub inline). Reference this class for dashboard tests

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,24 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { RouterLinkDirectiveStub } from '../../testing/router-link-directive-stub';
+import { HeroService } from '../hero.service';
+import { HeroServiceStub } from '../../testing/hero-service-stub';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  const heroServiceStub: HeroServiceStub = new HeroServiceStub();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [
+        DashboardComponent,
+        RouterLinkDirectiveStub
+       ],
+       providers: [{provide: HeroService, useValue: heroServiceStub}],
+       schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
---
+++ src/testing/hero-service-stub.ts
@@ -0,0 +1,7 @@
+import { Hero } from '../app/hero';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+export class HeroServiceStub {
+    getHeroes(): Observable<Hero[]> { return of([] as Hero[]); }
+}
