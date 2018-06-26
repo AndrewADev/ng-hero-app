052053839c208c294384b02e294c83ff183a7833
Andrew A <AndrewADev@gmail.com>
2018-06-25 20:43:22 -0400

    Fix hero serach component test
    Add missing dependencies for hero search component's tests

--- src/app/hero-search/hero-search.component.spec.ts
+++ src/app/hero-search/hero-search.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeroSearchComponent } from './hero-search.component';
+import { RouterLinkDirectiveStub } from '../../testing/router-link-directive-stub';
+import { HeroServiceStub } from '../../testing/hero-service-stub';
+import { HeroService } from '../hero.service';
 
 describe('HeroSearchComponent', () => {
   let component: HeroSearchComponent;
@@ -8,7 +11,13 @@ describe('HeroSearchComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroSearchComponent ]
+      declarations: [
+        HeroSearchComponent,
+        RouterLinkDirectiveStub
+      ],
+      providers: [
+        {provide: HeroService, useValue: HeroServiceStub}
+      ]
     })
     .compileComponents();
   }));
