5d8c443d6516d990a37ace113fe479b2641e80b5
Andrew A <AndrewADev@gmail.com>
2018-12-06 20:55:05 -0500

    Remove unneeded interface from component for static license text

--- src/app/static/static.component.ts
+++ src/app/static/static.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-static',
   templateUrl: './static.component.html',
   styleUrls: ['./static.component.css']
 })
-export class StaticComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class StaticComponent {
 }
